e9d5c7de3d52b0910f8955d96ab1b206ec85ed87
Juan Fernández <juan@bitomica.com>
2025-04-03 13:09:17 +0200

    trouble nvim added

--- init.lua
+++ init.lua
@@ -181,6 +181,43 @@ require("lazy").setup({
           }
         })
       end
+    },
+    {
+      "folke/trouble.nvim",
+      opts = {}, -- for default options, refer to the configuration section for custom setup.
+      cmd = "Trouble",
+      keys = {
+        {
+          "<leader>xx",
+          "<cmd>Trouble diagnostics toggle<cr>",
+          desc = "Diagnostics (Trouble)",
+        },
+        {
+          "<leader>xX",
+          "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+          desc = "Buffer Diagnostics (Trouble)",
+        },
+        {
+          "<leader>cs",
+          "<cmd>Trouble symbols toggle focus=false<cr>",
+          desc = "Symbols (Trouble)",
+        },
+        {
+          "<leader>cl",
+          "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+          desc = "LSP Definitions / references / ... (Trouble)",
+        },
+        {
+          "<leader>xL",
+          "<cmd>Trouble loclist toggle<cr>",
+          desc = "Location List (Trouble)",
+        },
+        {
+          "<leader>xQ",
+          "<cmd>Trouble qflist toggle<cr>",
+          desc = "Quickfix List (Trouble)",
+        },
+      },
     }
   },
   -- Configure any other settings here. See the documentation for more details.
@@ -209,7 +246,7 @@ require'lspconfig'.lua_ls.setup{}
 require'lspconfig'.astro.setup{}
 require'lspconfig'.clangd.setup{
   cmd = { "clangd", "--background-index", "--clang-tidy", "--suggest-missing-includes" },
-  filetypes = { "c", "cpp", "objc", "objcpp", "h", "hpp" },
+  filetypes = { "c", "cc", "hh", "cpp", "objc", "objcpp", "h", "hpp" },
   root_dir = require'lspconfig'.util.root_pattern(
     'Makefile',
     'compile_commands.json', -- Common for CMake projects
